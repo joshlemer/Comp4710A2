961071774204c66af203b19e1fe8e01659082761
Josh Lemer <joshlemer@gmail.com>
2014-10-26 17:15:25 -0500

    Initial commit, load_dataset() appears to work

---
+++ myAssociationMining.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Imports
+import apriorialg as ap
+import numpy as np      
+import arff
+
+# Read the data
+dataset = list()
+
+
+# When you get to part 7. you might wish to come back and
+# filter the data here - you could optionally have a variable
+# that is a flag to turn this filtering on and off
+
+
+# Find frequent itemsets - consider apriorialg.py
+
+
+# Generate rules - consider apriorialg.py
+
+
+
+# Generate interesting rules (Lift,Interest, PS, Phi)
+print ''
+print 'Interesting Rules:'
+        
+        
+        
---
+++ test.py
@@ -0,0 +1,5 @@
+import apriorialg as ap
+
+print ap.load_dataset()[0]
+print ap.load_dataset()[1]
+print ap.load_dataset()[29]
