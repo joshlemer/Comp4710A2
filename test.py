60f00e5247f23f5cea97281f3eb12538145ce316
Josh Lemer <joshlemer@gmail.com>
2014-10-29 20:18:27 -0500

    test to be incorp'd to apriorialg

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import apriorialg as ap
 
-print ap.load_dataset()[0]
-print ap.load_dataset()[1]
-print ap.load_dataset()[29]
+a = ap.load_dataset()
+b,c = ap.apriori(a, 0.002)
+d = ap.generateRules(b,c, 0.002)
+
+f = ap.filter_by_lift(c,d)
+f = ap.filter_by_interest(c,f)
+f = ap.filter_by_ps(c,f, 0.0035)
+f = ap.filter_by_phi(c,f, 0.35)
+ap.print_rules(f)
+
+
